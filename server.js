14b68a9d54ecc78c4195dc30dac13ba479aaf414
franzosterkamp <franz.osterkamp@gmx.net>
2019-12-05 11:10:57 +0100

    add express functions

--- server.js
+++ server.js
@@ -1,32 +1,68 @@
 const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
-
-// Connection URL
+const express = require("express");
+const app = express();
 const url = "mongodb://localhost:27017";
-
-// Database Name
-const dbName = "intro";
-
-// Create a new MongoClient
+const dbName = "clubs";
 const client = new MongoClient(url);
 
-// Use connect method to connect to the Server
+const port = 9090;
+app.use(express.static("public"));
+app.use(express.json());
 
-async function initDb() {
+// app.get("/clubs", async response => {
+//   // await client.connect();
+//   // const db = client.db(dbName);
+//   // const cursor = db.collection("clubs").find();
+//   // const clubs = await cursor.toArray();
+//   // console.log(clubs);
+//   response.end("hallo");
+
+//   // client.close();
+// });
+
+app.get("/api/clubs", async (request, response) => {
+  try {
+    const clubs = await getClubs();
+    response.end(clubs);
+  } catch (error) {
+    response.status(400).end("Can not read clubs");
+  }
+});
+
+app.get("/api/clubs/:key", async (request, response) => {
+  try {
+    const { key } = request.params;
+    const clubs = await getClub(key);
+    response.end(clubs);
+  } catch (error) {
+    response.status(400).end("Can not read clubs");
+  }
+});
+
+async function getClubs() {
   await client.connect();
   console.log("Connected successfully to server");
   const db = client.db(dbName);
-
-  const cursor = db.collection("users").find();
-  const users = await cursor.toArray();
-  console.log(users);
-
-  const cursor2 = db.collection("users").find({ firstName: "Franz" });
-  const users2 = await cursor2.toArray();
-
-  console.log(users2);
-
-  client.close();
+  const cursor = db.collection("clubs").find({});
+  const clubs = await cursor.toArray();
+  jsonClubs = JSON.stringify(clubs);
+  console.log(clubs);
+  return jsonClubs;
 }
 
-initDb();
+async function getClub(key) {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  console.log(key);
+  const Ikey = parseInt(key);
+  const cursor = db.collection("clubs").find({ id: Ikey });
+  const clubs = await cursor.toArray();
+  jsonClubs = JSON.stringify(clubs);
+  console.log(clubs);
+  return jsonClubs;
+}
+
+app.listen(port, () => {
+  console.log("Server Running!");
+});
